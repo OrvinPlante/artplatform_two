9d199d2286bfd267a65ec75624d370fd624f8a4d
Jay <jay@jay>
2023-01-15 22:50:48 +0100

    x2

--- src/Tools.js
+++ src/Tools.js
@@ -7,34 +7,34 @@ export default function Tools() {
 }
 
 function toDataURL(src, callback, outputFormat) {
-    let image = new Image();
-    image.crossOrigin = 'Anonymous';
-    image.onload = function () {
-      console.log("inside onload");
-      let canvas = document.createElement('canvas');
-      let ctx = canvas.getContext('2d');
-      let dataURL;
-      canvas.height = this.naturalHeight;
-      canvas.width = this.naturalWidth;
-      ctx.drawImage(this, 0, 0);
-      dataURL = canvas.toDataURL(outputFormat);
-      console.log("bottom of onload");
-      callback(dataURL);
-    };
+  let image = new Image();
+  image.crossOrigin = 'Anonymous';
+  image.onload = function () {
+    console.log("inside onload");
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    let dataURL;
+    canvas.height = this.naturalHeight;
+    canvas.width = this.naturalWidth;
+    ctx.drawImage(this, 0, 0);
+    dataURL = canvas.toDataURL(outputFormat);
+    console.log("bottom of onload");
+    callback(dataURL);
+  };
+  image.src = src;
+  if (image.complete || image.complete === undefined) {
+    image.src = "data:image/gif;base64, R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
     image.src = src;
-    if (image.complete || image.complete === undefined) {
-      image.src = "data:image/gif;base64, R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
-      image.src = src;
-      console.log("inside if");
-    }
+    console.log("inside if");
   }
-  function callToDataURL() {
-    let testURL = "https://www.gravatar.com/avatar/0c6523b4d3f60hj548962bfrg2350";
-    let prodURL = "";
-    console.log("callTo called");
-    toDataURL(prodURL,
+}
+function callToDataURL() {
+  let testURL = "https://www.gravatar.com/avatar/0c6523b4d3f60hj548962bfrg2350";
+  let prodURL = "";
+  console.log("callTo called");
+  toDataURL(prodURL,
     function (dataUrl) {
       console.log('RESULT:', dataUrl);
     }
-    );
-  }
+  );
+}
