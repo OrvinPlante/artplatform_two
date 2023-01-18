2701e94da7dd6269396cd1c5249b4d7f50809608
OrvinPlante <OrvinPlante@Github>
2023-01-18 23:19:03 +0100

    bootstrap grid, css

---
+++ src/Angebote.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function Angebote() {
+  return (
+    <>
+    <div className="angebote-grid">
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        <div className="col">col</div>
+        
+    </div>
+    </>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -2,44 +2,18 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import DatabaseJS from './Database';
+import Angebote from './Angebote';
 
 function App() {
   return (
     <>
-    <div className="container-fluid main-container">
-      <div className="row">
-        <div className="col sidebar-left">
-          
-        </div>
-        <div className="col-8 middle-container">
+    <div className="main-container">
+      <div className="sidebar-left">
+        <h2>Sidebar</h2>
+      </div>
+        <div className="middle-container">
           <h2>Gebrauchtkunsthandel JASAX</h2>
-          <div className="angebote-container">
-            <div className="row align-items-center">
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="w-100"></div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="w-100"></div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="w-100"></div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="w-100"></div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-              <div className="col">col</div>
-            </div>
-          </div>
-        </div>
-        <div className="col sidebar-right">
-
-        </div>
+          <Angebote />
       </div>
     </div>
     </>
